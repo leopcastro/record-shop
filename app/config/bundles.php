5f31113f93059d37296115c73de8201b8a3254fe
Leonardo Castro <leonardopcastro@gmail.com>
2020-11-16 01:06:50 +0100

    Add automatic generation of documentation for API
    Twig and Asset packages are necessary for the OpeanAPI UI

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
